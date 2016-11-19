d7f93cc9df9e00dfa94cf71de2558610b60da55b
sergiores24 <sergiores24@gmail.com>
2016-11-19 17:04:31 -0500

    Separación de datos
    50% Training
    25% Validation
    25% Test

--- HeartDisease.py
+++ HeartDisease.py
@@ -1,4 +1,6 @@
 import numpy as np
+import random
+
 
 def main():
 
@@ -14,4 +16,31 @@ def main():
         data.append(line)
 
     x = np.matrix(data)
+
+    [m, n] = x.shape
+    t = random.sample(range(m), round((m * 0.5)))
+    training_ds = x[t, :]
+
+    t2 = []
+    for element in range(m):
+        if element not in t:
+            t2.append(element)
+
+    x_half = x[t2, :]
+    [m, n] = x_half.shape
+    t = random.sample(range(m), round((m * 0.5)))
+    validation_ds = x_half[t, :]
+    t2 = []
+    for element in range(m):
+        if element not in t:
+            t2.append(element)
+    test_ds=x_half[t2,:]
+
+    y_training=training_ds[:,n-1]
+    y_validation=validation_ds[:,n-1]
+    y_test=test_ds[:,n-1]
+
+
+
+
 main()
