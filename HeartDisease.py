cb4e4fc6aaa141a1037b706dbfcb2e20069186fb
sergiores24 <sergiores24@gmail.com>
2016-11-19 19:04:15 -0500

    Predice

--- HeartDisease.py
+++ HeartDisease.py
@@ -1,6 +1,23 @@
 import numpy as np
 import random
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
+def featureNormalize(X):
+
+    [m,n] = X.shape
+
+    mean = X.mean(0)
+    std = np.amax(X, axis= 0) - np.amin(X, axis= 0)
+
+    X = (X-mean)/std
+
+    return [X,mean,std]
+
+def testNormalize(Xt,mean,sigma):
+    Xt = (Xt-mean)/sigma
+    return (Xt)
 
 def main():
 
@@ -40,7 +57,13 @@ def main():
     y_validation=validation_ds[:,n-1]
     y_test=test_ds[:,n-1]
 
+    [training_ds,mean,sigma]=featureNormalize(training_ds) #Normalizar datos del conjunto de entrenamiento
+    validation_ds = testNormalize(validation_ds,mean,sigma)
+    test_ds = testNormalize(test_ds,mean,sigma)
 
+    logreg = LogisticRegression(C=1e5)
+    logreg.fit(X=training_ds,y=np.ravel(y_training))
+    predict = logreg.predict(validation_ds)
 
 
 main()
