2c133422e96021c00529610ec2d70dc99ef7e254
Tetrimino <sergiores24@gmail.com>
2016-11-21 16:29:09 -0500

    gjh
    gjh

--- HeartDisease.py
+++ HeartDisease.py
@@ -3,16 +3,52 @@ import random
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score, KFold
 import sklearn.metrics as mt
+from sklearn import svm
+import os
+
+def accuracy (TP, TN, P, N):
+    return ((TP+TN)/(P+N))
+
+def sensitivity (TP,P):
+    return (TP/P)
+def specificity (TN,N):
+    return (TN/N)
+def F1Score(TP,FP,FN):
+    return ((2*TP)/((2*TP)+FP+FN))
 
 def LogisticR():
     logreg = LogisticRegression(C=5)
     logreg.fit(X=training_ds,y=np.ravel(y_training))
-    print(cross_val_score(logreg, validation_ds, y=np.ravel(y_validation), cv = 5))
+    score = cross_val_score(logreg, validation_ds, y=np.ravel(y_validation), cv = 5)
+    print("\nCV Accuracy LogisticRegression = ",np.mean(score)*100)
     predict=logreg.predict(test_ds)
-    print(mt.confusion_matrix(predict,y_test))
+    Cmatrix = mt.confusion_matrix(predict,y_test)
+    print ("\n--------------------------Confusion Matrix LogisticRegression -------------------------")
+    print ("                    || Predicted Condition Positive || Predicted Condition Negative || \n"
+           "Condition Positive  ||             ",Cmatrix[0,0],"             ||          ",Cmatrix[0,1], "                 || ",
+           "\nCondition Negative  ||             ",Cmatrix[1,0],"              ||          ",Cmatrix[1,1], "                || ")
+    print("\nAccuracy: ",accuracy(Cmatrix[0,0],Cmatrix[1,1],(Cmatrix[0,0]+Cmatrix[0,1]),(Cmatrix[1,0]+Cmatrix[1,1])))
+    print("Sensitivity: ",sensitivity(Cmatrix[0,0],(Cmatrix[0,0]+Cmatrix[0,1])))
+    print("Specificity: ",specificity(Cmatrix[1,1],(Cmatrix[1,1]+Cmatrix[1,0])))
+    print("F1Score: ", F1Score(Cmatrix[0,0],Cmatrix[1,0],Cmatrix[0,1]))
 
 def SupportVM():
-    
+
+    clf = svm.SVC(C= 1,kernel='poly',degree=3)
+    clf.fit(X = training_ds, y = np.ravel(y_training))
+    score = cross_val_score(clf, validation_ds, y=np.ravel(y_validation), cv = 5)
+    print("\nCV Accuracy SVM = ",np.mean(score)*100)
+    predict = clf.predict(test_ds)
+    Cmatrix = mt.confusion_matrix(predict,y_test)
+    print ("\n--------------------------Confusion Matrix SVM--------------- -------------------------")
+    print ("                    || Predicted Condition Positive || Predicted Condition Negative || \n"
+           "Condition Positive  ||             ",Cmatrix[0,0],"             ||          ",Cmatrix[0,1], "                 || ",
+           "\nCondition Negative  ||             ",Cmatrix[1,0],"              ||          ",Cmatrix[1,1], "                || ")
+    print("\nAccuracy: ",accuracy(Cmatrix[0,0],Cmatrix[1,1],(Cmatrix[0,0]+Cmatrix[0,1]),(Cmatrix[1,0]+Cmatrix[1,1])))
+    print("Sensitivity: ",sensitivity(Cmatrix[0,0],(Cmatrix[0,0]+Cmatrix[0,1])))
+    print("Specificity: ",specificity(Cmatrix[1,1],(Cmatrix[1,1]+Cmatrix[1,0])))
+    print("F1Score: ", F1Score(Cmatrix[0,0],Cmatrix[1,0],Cmatrix[0,1]))
+
 
 def main():
 
@@ -52,6 +88,8 @@ def main():
     y_training=training_ds[:,n-1]
     y_validation=validation_ds[:,n-1]
     y_test=test_ds[:,n-1]
-    logisticr()
+    LogisticR()
+    #wait = input()
+    SupportVM()
 
 main()
